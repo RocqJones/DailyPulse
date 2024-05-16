859621e5d1d3d93bd844ee1182a48154289f4679
RocqJones <jonesmbindyo@gmail.com>
2024-05-17 00:31:32 +0300

    introduced navigation to About screen and backstack logic

--- androidApp/src/main/java/com/rocqjones/dailypulse/android/screens/ArticlesScreen.kt
+++ androidApp/src/main/java/com/rocqjones/dailypulse/android/screens/ArticlesScreen.kt
@@ -10,8 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,23 +24,26 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.rocqjones.dailypulse.android.R
 import com.rocqjones.dailypulse.articles.ArticleModel
 import com.rocqjones.dailypulse.articles.ArticlesViewModel
 
 @Composable
 fun ArticlesScreen(
+    onAboutButtonClick: () -> Unit,
     articlesViewModel: ArticlesViewModel
 ) {
     val articleState = articlesViewModel.articlesState.collectAsState()
 
     Column {
-        AppBar()
+        AppBar(onAboutButtonClick)
 
         if (articleState.value.loading)
             Loader()
@@ -51,8 +58,19 @@ fun ArticlesScreen(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun AppBar() {
-    TopAppBar(title = { Text(text = "Articles") })
+private fun AppBar(onAboutButtonClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = "Articles") },
+        // actions
+        actions = {
+            IconButton(onClick = onAboutButtonClick) {
+                Icon(
+                    imageVector = Icons.Outlined.Info,
+                    contentDescription = stringResource(R.string.about_device_button)
+                )
+            }
+        }
+    )
 }
 
 @Composable
